58950855fb268f4b223b1ba116ce2e8ac931c442
Wesley Salmi <wsalmi@latam.stefanini.com>
2020-02-03 16:55:41 -0300

    Adicionando novo evento ExibirHistórico

---
+++ Servidor/Lampadas/Controllers/HistoricoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Lampadas.Controllers
+{
+    [RoutePrefix("api/historico")]
+    public class HistoricoController : ApiController
+    {
+        // GET: Historico
+        [HttpGet, Route("exibir")]
+        public IHttpActionResult Exibir()
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<MainHub>();
+
+            hubContext.Clients.All.verHistorico(true);
+
+            return Ok(new { msg = "🤘" });
+        }
+
+        [HttpGet, Route("ocultar")]
+        public IHttpActionResult Ocultar()
+        {
+            var hubContext = GlobalHost.ConnectionManager.GetHubContext<MainHub>();
+
+            hubContext.Clients.All.verHistorico(false);
+
+            return Ok(new { msg = "🐱‍👤" });
+        }
+    }
+}
